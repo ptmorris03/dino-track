7b63b676c67f00bfd4c9057f545c28091cf72b5f
Paul Morris <ptmorris03@gmail.com>
2024-05-02 01:30:38 -0400

    ++

--- dinotrack/settings.py
+++ dinotrack/settings.py
@@ -2,7 +2,7 @@ import torch
 
 
 DEFAULT_MODEL = "facebook/dinov2-small"
-DEFAULT_WIDTH = 504
-DEFAULT_HEIGHT = 504
+DEFAULT_WIDTH = 1792
+DEFAULT_HEIGHT = 1792
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 DTYPE = torch.float16 if torch.cuda.is_available() else torch.float32
